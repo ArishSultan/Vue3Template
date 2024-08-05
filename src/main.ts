eb3303fef3ca4bfa28913b90961ddabd9b893c42
ArishSultan <arishgit000@gmail.com>
2024-08-05 16:18:45 +0500

    Setup Sentry

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,23 @@ import router from './router';
 import store from './store';
 import vuetify from './vuetify';
 
-createApp(App).use(store).use(router).use(vuetify).mount('#app');
+import * as Sentry from '@sentry/vue';
+import { isDevMode } from '@/utils/env';
+
+const vueApp = createApp(App);
+
+Sentry.init({
+    app: vueApp,
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration()
+    ],
+    tracesSampleRate: 1.0,
+    // tracePropagationTargets: ['http://localhost:5173', 'https://example.com'],
+    replaysSessionSampleRate: isDevMode ? 1.0 : 0.0,
+    replaysOnErrorSampleRate: 1.0,
+    profilesSampleRate: 1.0,
+});
+
+vueApp.use(store).use(router).use(vuetify).mount('#app');
